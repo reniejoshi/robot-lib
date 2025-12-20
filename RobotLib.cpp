ec1e8d89221f2efff8a79ef7b85129c70ee5a98a
reniejoshi <renie_joshi@yahoo.com>
2025-12-20 13:19:21 -0800

    Initial commit

---
+++ RobotLib.cpp
@@ -0,0 +1,2 @@
+#include "RobotLib.h"
+#include <Arduino.h>
---
+++ RobotLib.h
@@ -0,0 +1,20 @@
+#ifndef ROBOT_LIB_H
+#define ROBOT_LIB_H
+
+class Subsystem {
+
+};
+
+class Command {
+
+};
+
+class CommandScheduler final {
+
+};
+
+class RobotBase {
+
+};
+
+#endif
