7becec14c6a2b2fc5f2d59031a647bebd6f5105f
reniejoshi <renie_joshi@yahoo.com>
2025-12-20 14:30:10 -0800

    Add RobotLib namespace and implement Servo class
    Implement Servo constructor to attach pin and method to clamp and set angle.

--- RobotLib.cpp
+++ RobotLib.cpp
@@ -1,2 +1,14 @@
 #include "RobotLib.h"
 #include <Arduino.h>
+#include <Servo.h>
+
+namespace RobotLib {
+    Servo::Servo(int pin) {
+        servoLib.attach(pin);
+    }
+
+    void Servo::setAngle(double angle) {
+        angle = constrain(angle, 0, 180);
+        servoLib.write(angle);
+    }
+}
--- RobotLib.h
+++ RobotLib.h
@@ -1,20 +1,32 @@
 #ifndef ROBOT_LIB_H
 #define ROBOT_LIB_H
 
-class Subsystem {
+#include <Servo.h>
 
-};
+namespace RobotLib {
+    class Subsystem {
 
-class Command {
+    };
 
-};
+    class Command {
 
-class CommandScheduler final {
+    };
 
-};
+    class CommandScheduler final {
 
-class RobotBase {
+    };
 
-};
+    class RobotBase {
 
+    };
+
+    class Servo {
+        public:
+            Servo(int pin);
+            void setAngle(double angle);
+        
+        private:
+            ::Servo servoLib;
+    };
+}
 #endif
