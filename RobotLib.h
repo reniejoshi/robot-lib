e1888a39c9d56688e6935a444024a9b9767b70ce
reniejoshi <renie_joshi@yahoo.com>
2026-02-09 22:45:55 -0800

    Add fields and method declarations

--- RobotLib.h
+++ RobotLib.h
@@ -2,18 +2,36 @@
 #define ROBOT_LIB_H
 
 #include <Servo.h>
+#include <Vector.h>
 
 namespace RobotLib {
     class Subsystem {
-
+        public:
+            virtual void periodic();
     };
 
     class Command {
-
+        public:
+            virtual void initialize();
+            virtual void execute();
+            virtual void end(bool interrupted);
+            virtual bool isFinished();
+            void addRequirements(Subsystem* requirements);
+            void schedule();
+        
+        private:
+            Subsystem* requirement;
     };
 
     class CommandScheduler final {
+        public:
+            static CommandScheduler& getInstance();
+            void run();
+            void schedule();
 
+        private:
+            CommandScheduler& instance;
+            Vector<Command> scheduledCommands;
     };
 
     class RobotBase {
